545ef61752314069d3711244304baca055369483
Ben Roberts <ben@bouncingsprout.com>
2016-12-09 22:30:32 +0000

    First Steps

---
+++ WP_Estimation_Form_Client_Interface.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: WP Estimation Form Client interface
+Plugin URI:  https://www.bouncingsprout.com
+Description: A client interface for the WP Cost Estimation and Payment Forms Builder Plugin
+Version:     1.0
+Author:      Ben Roberts
+Author URI:  https://www.bouncingsprout.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wporg
+Domain Path: /languages
+*/
